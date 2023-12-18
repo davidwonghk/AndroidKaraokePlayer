971e9b9f62ed92c82c6ef900d12895f28b29b58b
David Wong <davidwongtt@gmail.com>
2023-12-18 01:09:13 +0000

    add script

---
+++ call_build_ffmpeg.sh
@@ -0,0 +1,1 @@
+./build_ffmpeg.sh ~/workspace/media/libraries/decoder_ffmpeg/src/main  ~/Android/Sdk/ndk/26.1.10909125 linux-x86_64 mpeg4 msmpeg4v1 msmpeg4v2 msmpeg4v3 mp1 mp2 mp3 mp4als mpegts mpegh_3d_audio
